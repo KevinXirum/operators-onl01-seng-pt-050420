16f7ce50a55af4b952d3190321f2456b41ef7299
kevin xirum <kevinmenjivar66@gmail.com>
2020-05-06 22:33:59 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -12,7 +12,7 @@ end
 
 
 def not_safe?(speed)
-  if speed is greater than 60 or if speed less than 40 unless speed is between 40 && 60 ? true : false
+  if speed is greater than 60 or if speed less than 40 ? true : false
 	end
 end
 
